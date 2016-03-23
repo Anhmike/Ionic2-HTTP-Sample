d0ffa9223d698d3be252955953969023a87f1141
srehanuddin <s.rehanuddin@gmail.com>
2016-03-23 18:19:49 +0500

    API Impliment

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,6 +1,36 @@
 import {Page} from 'ionic-angular';
+import { Http } from 'angular2/http';
+
+class PostModel {
+    userId : number;
+    id : number;
+    title : string;
+    completed : boolean;
+}
 
 @Page({
   templateUrl: 'build/pages/home/home.html'
 })
-export class HomePage {}
+export class HomePage {
+    url = "http://jsonplaceholder.typicode.com/";
+    
+    list : Array<PostModel>;
+    
+    constructor(public http : Http){
+        
+    }
+    
+    makeHttpRequest(){
+        
+        console.log("Make Request");
+        
+        this.http.request(this.url + "posts")
+            .subscribe((res)=>{
+                console.log(res.json())
+                this.list = res.json();
+            }, (err)=>{
+                console.log(err)
+            })
+    }
+}
+
